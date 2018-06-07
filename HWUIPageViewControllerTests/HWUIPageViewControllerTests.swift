f43247ebdd9cf788abeb8d08544acac920cc5726
mikhey <mikheenkovladislav@gmail.com>
2018-06-07 08:16:10 +0300

    Initial Commit

---
+++ HWUIPageViewControllerTests/HWUIPageViewControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  HWUIPageViewControllerTests.swift
+//  HWUIPageViewControllerTests
+//
+//  Created by mikhey on 07.06.2018.
+//  Copyright © 2018 mikhey. All rights reserved.
+//
+
+import XCTest
+@testable import HWUIPageViewController
+
+class HWUIPageViewControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
